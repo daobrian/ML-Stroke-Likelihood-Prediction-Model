f1ce27a2ac9ed9182de8fd7c5b41a55e28436897
Brian Dao <dbrianv8@gmail.com>
2023-11-29 15:23:32 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import streamlit.web.cli as cli
+
+import numpy as np
+import pickle
+import sklearn
+
+
+loaded_model = pickle.load(open('model_and_scaler.pkl', 'rb'))
+
+smoke_dict = {'never' : 0, 'former' : 1, 'smokes' : 2}
+yes_no_dict = {'no' : 0, 'yes' : 1}
+
+st.title('Stroke Likelihood Prediction	:brain:')
+age = st.slider("Choose age", 0, 100)
+gender = st.radio("Choose gender", ['male','female','other'])
+bmi = st.slider("Choose BMI", 0, 100)
+glucose = st.slider("CHoose average glucose level in blood (mg/dL)", 0, 300)
+hypertension = st.select_slider("Patient has hypertension", {'no' : 0, 'yes' : 1})
+married = st.select_slider("Married", {'no' : 0, 'yes' : 1})
+work = st.select_slider("Work type", ['never worked', 'children', 'self employed', 'private', 'govt job'])
+residence = st.radio("Choose residence type", ['Rural', 'Urban'])
+smoke = st.radio("Choose smoking status", ['never', 'former', 'smokes'])
+heart = st.select_slider("Patient has heart disease", {'no' : 0, 'yes' : 1})
+
+
+def predict():
+    transformed_data = loaded_model['scaler'].transform([[age,bmi,glucose]])[0]
+    rw = [[transformed_data[0], yes_no_dict[hypertension], yes_no_dict[heart], yes_no_dict[married], transformed_data[2], transformed_data[1], smoke_dict[smoke], int(work == 'govt job'), int(work == 'never worked'), int(work == 'private'), int(work == 'self employed'), int(work == 'children'), int(residence == 'Rural'), int(residence == 'Urban'), int(gender == 'female'), int(gender == 'male'), int(gender == 'other')]]
+
+    prediction = loaded_model['model'].predict(rw)
+    prob = loaded_model['model'].predict_proba(rw)[0]
+
+    if prediction[0] == 1: 
+        st.error(f'Patient at risk for stroke | Probability = {np.round(prob[1] * 100, 2)}%')
+    else: 
+        st.success(f'Patient is not at risk for stroke | Probability = {np.round(prob[0] * 100, 2)}%') 
+
+trigger = st.button('Predict', on_click=predict)
