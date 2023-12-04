abf262cb6a02d3ac0b6f1993e5f2b8947b0a90cb
daobrian <brdao@ucdavis.edu>
2023-12-04 00:55:58 -0800

    app is now produces results dynamically instead of button

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import numpy as np
 import pickle
 import sklearn
 
-
 loaded_model = pickle.load(open('model_and_scaler.pkl', 'rb'))
 
 smoke_dict = {'never' : 0, 'former' : 1, 'smokes' : 2}
 yes_no_dict = {'no' : 0, 'yes' : 1}
 
 st.title('Stroke Likelihood Prediction	:brain:')
+st.markdown('This app is powered by a logistic regression classifier optimized for recall.')
 age = st.slider("Choose age", 0, 100)
 bmi = st.slider("Choose BMI", 0, 100)
 glucose = st.slider("Choose average glucose level in blood (mg/dL)", 0, 300)
@@ -21,13 +21,8 @@ work = st.select_slider("Work type", ['never worked', 'children', 'self employed
 smoke = st.radio("Choose smoking status", ['never', 'former', 'smokes'])
 heart = st.radio("Patient has heart disease", ['no', 'yes'])
 
-# Features removed after feature selection
-# gender = st.radio("Choose gender", ['male','female','other'])
-# residence = st.radio("Choose residence type", ['Rural', 'Urban'])
-
 def predict():
     transformed_data = loaded_model['scaler'].transform([[age,bmi,glucose]])[0]
-    #rw = [[transformed_data[0], yes_no_dict[hypertension], yes_no_dict[heart], yes_no_dict[married], transformed_data[2], transformed_data[1], smoke_dict[smoke], int(work == 'govt job'), int(work == 'never worked'), int(work == 'private'), int(work == 'self employed'), int(work == 'children'), int(residence == 'Rural'), int(residence == 'Urban'), int(gender == 'female'), int(gender == 'male'), int(gender == 'other')]]
     rw = [[transformed_data[0], yes_no_dict[hypertension], yes_no_dict[heart], yes_no_dict[married], transformed_data[2], transformed_data[1], smoke_dict[smoke], int(work == 'govt job'), int(work == 'never worked'), int(work == 'private'), int(work == 'self employed'), int(work == 'children')]]
     prediction = loaded_model['model'].predict(rw)
     prob = loaded_model['model'].predict_proba(rw)[0]
@@ -37,4 +32,4 @@ def predict():
     else: 
         st.success(f'Patient is not at risk for stroke | Probability = {np.round(prob[0] * 100, 2)}%') 
 
-trigger = st.button('Predict', on_click=predict)
+predict()
